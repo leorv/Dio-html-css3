a96ae5be3bc4d1d3fc6c458951ee0483025e0c1f
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-01-16 09:20:56 -0300

    typescript interfaces

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,3 +1,35 @@
 // Inicia em http://localhost:1234/
 
 console.log('Hello World, TypeScript!');
+
+// we can type our variables.
+function sum(a: number, b: number) {
+    return a + b;
+}
+sum(2, 2);
+
+// Interfaces
+interface IAnimal {
+    name: string,
+    type: 'terrestrial' | 'aquatic'
+}
+
+const animal: IAnimal = {
+    name: 'Elefante',
+    type: 'terrestrial'
+};
+
+interface IFeline extends IAnimal {
+    nightVision: boolean,
+    roar(level: number): void
+};
+
+const feline: IFeline = {
+    name:'Leão',
+    type: 'terrestrial',
+    nightVision: false,
+
+    roar: (level) => (`${level}dB`) 
+}
+
+feline.roar(10);
