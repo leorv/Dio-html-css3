a92949cf04bc20a54127fbdfe0f64fd8e1416e6a
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-01-16 10:32:27 -0300

    html element

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,50 +1,6 @@
-// Inicia em http://localhost:1234/
+const input = document.getElementById('input') as HTMLInputElement;
 
-console.log('Hello World, TypeScript!');
-
-// we can type our variables.
-function sum(a: number, b: number) {
-    return a + b;
-}
-sum(2, 2);
-
-// Interfaces
-
-interface IAnimal {
-    name: string,
-    type: 'terrestrial' | 'aquatic'
-}
-
-const animal: IAnimal = {
-    name: 'Elefante',
-    type: 'terrestrial'
-};
-
-interface IFeline extends IAnimal {
-    nightVision: boolean,
-    roar(level: number): void
-};
-
-interface ICanine extends IAnimal {
-    isFriendly: boolean
-}
-
-const feline: IFeline = {
-    name:'Leão',
-    type: 'terrestrial',
-    nightVision: false,
-
-    roar: (level) => (`${level}dB`) 
-}
-
-feline.roar(10);
-
-// type
-
-type IDomestic = IFeline | ICanine; // can join too: &
-
-const animal2: IDomestic = {
-    name: 'Cachorro',
-    type: 'terrestrial',
-    isFriendly: true
-}
+input.addEventListener('input',(event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+})
