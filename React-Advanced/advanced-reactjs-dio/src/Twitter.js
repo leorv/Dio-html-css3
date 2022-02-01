895b4cc12014514a911bae544372a8389b741976
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-02-01 10:41:00 -0300

    react hooks

--- React-Advanced/advanced-reactjs-dio/src/Twitter.js
+++ React-Advanced/advanced-reactjs-dio/src/Twitter.js
@@ -1,45 +1,49 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState ,memo } from 'react';
 
-class Twitter extends Component {
+// Não use hooks dentro de funções JS comuns, em vez disso, você pode:
+// - Chamar hooks em componentes React.
+// - Chamar hooks dentro de hooks customizados.
+// Seguindo essas regras, você garante que toda lógica de estado (state)
+// no componente seja claramente visível no código fonte.
 
-    state = {
-        tweet: 'title'
-    }
-
-    componentDidMount() {
-        const { posts, loading } = this.props;
-        console.log('componentDidMount', posts);
-        console.log('componentDidMount: loading', loading)
-    }
-
-    componentDidUpdate(prevProps){
-        const { loading } = this.props;
-        if (this.props.loading !== prevProps.loading) {
-            console.log('componentDidupdate: loading', loading);
-        }        
-    }
-
-    componentWillUnmount(){
-        const posts = this.props;
-        console.log('componentWillUnmount', posts);
-    }
-
-    shouldComponentUpdate(nextProps, nextState){
-        // return this.state.tweet !== nextState.tweet || nextProps.loading !== this.props.loading;
-        console.log('shouldComponentUpdate', nextState, nextProps);
-        return true;        
-    }
-
-    render() {
-        const posts = this.props;
-        console.log('render', posts);
-
-        return (
-            <div>
-                tests
-            </div>
-        );
-    }
+// Evitar re-render, caso desnecessário.
+const areEqual = (prevProps, nextProps) => {
+    return prevProps.loading === nextProps.loading;
 }
 
-export default Twitter;
+function Twitter(props) {
+
+    const { loading } = props;
+    const [ tweet, setTweet ] = useState('title');
+
+    useEffect(() => {
+        console.log("componentDidMount");
+    }, []);
+
+    useEffect(() => {
+        console.log("componentDidUpdate");
+    }, [loading]);
+
+    useEffect(() => {
+        // O return dentro do useEffect representa o componentWillUnmount
+        return () => {
+            console.log("compoenentWillUnmount");
+        }
+    }, []);
+
+    const handleTweet = () => {
+        setTweet('Tweet atualizado');
+    }
+
+    console.log('Tweet atualizado', tweet);
+
+    return (
+        <div>
+            <button onClick={handleTweet}>Re-render</button>
+            tests
+        </div>
+    );
+
+}
+
+export default memo(Twitter, areEqual);
