5e3207637c081934834e8ae9409e0c59e08c8f09
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-10-13 11:10:42 -0300

    Lendo arquivos e tratando exceções.

---
+++ dotnet/C#/Exceções e Coleções/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+
+}
+catch (FileNotFoundException e)
+{
+    System.Console.WriteLine($"Ocorreu um erro com a leitura do arquivo. Arquivo não encontrado. {e.Message}");
+}
+catch (DirectoryNotFoundException e)
+{
+    System.Console.WriteLine($"Ocorreu um erro com a leitura do arquivo. Diretório não encontrado. {e.Message}");
+}
+catch (Exception e)
+{
+    System.Console.WriteLine($"Ocorreu um exceção genérica. Mensagem: {e.Message}");
+}
+finally
+{
+    System.Console.WriteLine("Chegou até o fim.");
+}
---
+++ dotnet/C#/Exceções e Coleções/README.md
@@ -0,0 +1,1 @@
+# Exceções e Coleções
