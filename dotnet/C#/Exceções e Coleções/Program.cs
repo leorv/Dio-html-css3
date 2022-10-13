61bc32c2893addbd786f308d6c186298634980d3
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-10-13 12:45:51 -0300

    Coleções C#

--- dotnet/C#/Exceções e Coleções/Program.cs
+++ dotnet/C#/Exceções e Coleções/Program.cs
@@ -27,3 +27,84 @@ finally
 {
     System.Console.WriteLine("Chegou até o fim.");
 }
+
+// Fila
+
+Queue<int> fila = new Queue<int>();
+fila.Enqueue(23);
+fila.Enqueue(5);
+fila.Enqueue(76);
+fila.Enqueue(12);
+
+foreach (int item in fila) {
+    System.Console.WriteLine(item);
+}
+
+// removendo o primeiro elemento.
+System.Console.WriteLine($"Removendo o primeiro elemento: {fila.Dequeue()}");
+
+foreach (int item in fila)
+{
+    System.Console.WriteLine(item);
+}
+
+// Pilha
+System.Console.WriteLine("Pilha:");
+Stack<int> pilha = new Stack<int>();
+pilha.Push(4);
+pilha.Push(8);
+pilha.Push(34);
+pilha.Push(56);
+pilha.Push(84);
+
+foreach(int item in pilha) {
+    System.Console.WriteLine(item);
+}
+
+System.Console.WriteLine($"Removendo o último elemento: {pilha.Pop()}");
+
+
+foreach (int item in pilha)
+{
+    System.Console.WriteLine(item);
+}
+
+// Dictionary
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("PR", "Paraná");
+
+foreach(var item in estados) {
+    System.Console.WriteLine($"Sigla: {item.Key}, Nome: {item.Value}");
+}
+
+System.Console.WriteLine("Removendo PR");
+estados.Remove("PR");
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Sigla: {item.Key}, Nome: {item.Value}");
+}
+
+// Alterando o valor:
+// Somente o valor, não dá para alterar a chave.
+System.Console.WriteLine("Alterando o valor de São Paulo:");
+estados["SP"] = "Sãum Paulu";
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Sigla: {item.Key}, Nome: {item.Value}");
+}
+
+string chave = "BA";
+System.Console.WriteLine($"Verificando o elemento: {chave}");
+
+if (estados.ContainsKey(chave)) {
+    System.Console.WriteLine("Valor existente.");
+} else {
+    System.Console.WriteLine("Não existe, é seguro adicionar ele.");
+}
+
--- dotnet/C#/Exceções e Coleções/README.md
+++ dotnet/C#/Exceções e Coleções/README.md
@@ -1,1 +1,13 @@
 # Exceções e Coleções
+
+## Fila (Queue)
+
+FIFO - O primeiro que entra, é o primeiro que sai.
+
+## Pilha (Stack)
+
+LIFO - O último que entrou é o primeiro a sair.
+
+## Dicionário (Dictionary)
+
+É uma coleção de chave-valor para armazenar valores únicos sem ordem específica. A chave é única.
