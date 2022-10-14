be1a3d5409eb572b0e15033f6dd54368edc546d8
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-10-13 21:58:08 -0300

    Métodos de extensões. Extendendo o comportamento do inteiro.

--- dotnet/C#/Tipos especiais/Program.cs
+++ dotnet/C#/Tipos especiais/Program.cs
@@ -7,3 +7,12 @@ MeuArray<int> arrayInteiro = new MeuArray<int>();
 arrayInteiro.AdicionarElemento(30);
 
 System.Console.WriteLine(arrayInteiro[0]);
+
+int num = 20;
+bool par = false;
+
+par = num.EhPar();
+
+string msg = $"O número {num} é " + (par ? "par." : "ímpar.");
+
+System.Console.WriteLine(msg);
